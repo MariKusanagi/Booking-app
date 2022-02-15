03dc75e7a6cedb0004b411eadabfaf07fe313448
Mariana Duda <marianadudajava@gmail.com>
2022-02-15 14:29:36 +0100

    load

---
+++ go.mod
@@ -0,0 +1,3 @@
+module boocking-app
+
+go 1.17
---
+++ helper/helper.go
@@ -0,0 +1,13 @@
+package helper
+
+import "strings"
+
+var MyVar ="somevalue"
+
+func validateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := strings.Contains(email, "@")
+	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+	return isValidName, isValidEmail, isValidTicketNumber
+}
+
